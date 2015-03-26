5c97315e8342f3c1fd2762bf734574100f7aafa7
Skurishin Vladislav <vskurishin@marathonbet.ru>
2015-03-27 00:41:32 +0300

    Init libgdx project.

---
+++ android/src/com/spacecraft/game/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.spacecraft.game.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.spacecraft.game.SpaceCraft;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SpaceCraft(), config);
+	}
+}
---
+++ desktop/src/com/spacecraft/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.spacecraft.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.spacecraft.game.SpaceCraft;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpaceCraft(), config);
+	}
+}

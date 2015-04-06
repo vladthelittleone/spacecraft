5084074ae1a728b4d26f1ee3dd47a1aabc3b0244
Skurishin Vladislav <vskurishin@marathonbet.ru>
2015-04-07 00:18:03 +0300

    Add hello world example.

--- core/src/com/spacecraft/game/SpaceCraft.java
+++ core/src/com/spacecraft/game/SpaceCraft.java
@@ -2,26 +2,39 @@ package com.spacecraft.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class SpaceCraft extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+public class SpaceCraft extends ApplicationAdapter
+{
+    SpriteBatch batch;
+    BitmapFont font;
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
+    @Override
+    public void create()
+    {
+        batch = new SpriteBatch();
+        font = new BitmapFont(Gdx.files.internal("fonts/verdana39.fnt"),
+                Gdx.files.internal("fonts/verdana39.png"), false);
+        font.setColor(Color.RED);
+    }
+
+    @Override
+    public void render()
+    {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        font.draw(batch, "Hello World", 200, 200);
+        batch.end();
+    }
+
+    @Override
+    public void dispose()
+    {
+        batch.dispose();
+        font.dispose();
+    }
 }
--- ios/src/com/spacecraft/game/IOSLauncher.java
+++ ios/src/com/spacecraft/game/IOSLauncher.java
@@ -1,11 +1,9 @@
 package com.spacecraft.game;
 
-import org.robovm.apple.foundation.NSAutoreleasePool;
-import org.robovm.apple.uikit.UIApplication;
-
 import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
 import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
-import com.spacecraft.game.SpaceCraft;
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
 
 public class IOSLauncher extends IOSApplication.Delegate {
     @Override

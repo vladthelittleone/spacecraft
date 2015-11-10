e0c5d09eaa6ddbaaa3cf0fb4d511ec1d1b6b74ed
vaimer <overespada@gmail.com>
2015-11-10 13:12:18 +0300

    get code from storage

--- client/scripts/controllers/main.js
+++ client/scripts/controllers/main.js
@@ -24,7 +24,7 @@ angular.module('spacecraft.main', [])
         var editorSession,
             editorRenderer;
 
-        $scope.code = "return { \n\t" +
+        $scope.code = $storage.getItem("code") || "return { \n\t" +
                         "run : function(spaceCraft, world) \n\t" +
                         "{  \n\t\tspaceCraft.weapon.fire();  \n\t}  " +
                         "\n};";
@@ -59,8 +59,6 @@ angular.module('spacecraft.main', [])
         $scope.aceChanged = function ()
         {
             $scope.code = editorSession.getDocument().getValue();
-
-            $storage.local.removeItem("code");
             $storage.local.setItem("code", $scope.code);
         };
 

2746bf13cbf88d504cc97481b1b621612b37e1a7
Skurishin Vladislav <vskurishin@marathonbet.ru>
2016-01-05 21:51:59 +0300

    ch interpreter.

--- client/scripts/game/shared/Interpreter.js
+++ client/scripts/game/shared/Interpreter.js
@@ -1,39 +1,37 @@
-var Interpreter = function () {
+var Interpreter = function ()
+{
 	var that = {};
 	var array = that.array = [];
-
-	var print = function(value)
-	{
-		array.push(value);
-	};
-
-	var println = function(value)
-	{
-		array.push(value + '<br>');
-	};
-
-	var editCode = function(code)
-	{
-		return code + " array.toString().replace(new RegExp(',', 'g'), '');"
-	};
-
-	var runCode = function(code)
-	{
-		try
-		{
-			return eval(code);
-		}
-		catch (ex)
-		{
-			return ex.message;
-		}
-	};
 
 	that.execute = function (code)
 	{
 		// очищаем от старых значений
 		array = [];
-		return runCode(editCode(code));
+
+		try
+		{
+			/**
+			 * Функция для интерпретации.
+			 * Используется для вывода сообщения в окно BBot'а.
+			 */
+			var BBotDebug = function (value)
+			{
+				array.push(value);
+			};
+
+			var result = eval(code);
+
+			if (array.length)
+			{
+				return array;
+			}
+
+			return result;
+		}
+		catch (ex)
+		{
+			return ex.message;
+		}
 	};
 
 	return that;

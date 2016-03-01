852190b098297d14b50ba19f397cdd5226eb75ff
vaimer <overespada@gmail.com>
2016-03-02 00:05:04 +0300

    some tray

--- routes/statistic.js
+++ routes/statistic.js
@@ -2,12 +2,23 @@
  * Created by Ivan on 29.02.2016.
  */
 var express = require('express');
+var User = require('models/user').User;
+var Statistic = require('models/statistic').Statistic;
+var mongoose = require('utils/mongoose');
 var router = express.Router();
 
 router.post('/', function (req, res, next)
 {
 	if(req.session.user){
-
+		var user = User.findById(req.session.user);
+		var stat = new Movie({
+			username: user.username,
+			statistic: req.body
+		});
+		stat.save(function(err, stat){
+			if(err)
+				console.error(err);
+		});
 	}
 	res.send(200);
 });

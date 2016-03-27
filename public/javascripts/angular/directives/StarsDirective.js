49a2d470d90b8994a22dbe0b340a23868c0378e7
vaimer <overespada@gmail.com>
2016-03-27 23:01:48 +0300

    fix problems with directive

--- public/javascripts/angular/directives/StarsDirective.js
+++ public/javascripts/angular/directives/StarsDirective.js
@@ -1,14 +1,16 @@
+'use strict';
 /**
  * Created by Ivan on 23.03.2016.
  */
 var app = angular.module('spacecraft.stars', []);
 
-app.directive('spacecraft.stars', ['$http', '$state', function($http, $state)
+app.directive('stars', ['$http', '$state', function($http, $state)
 {
-	function link ()
+	function link ($scope)
 	{
 		$scope.RadioChange = function (value)
 		{
+			console.log("in function value = "+ value);
 			$http({
 				url: '/statistic/lessons/stars',
 				method: 'POST',
@@ -23,7 +25,6 @@ app.directive('spacecraft.stars', ['$http', '$state', function($http, $state)
 
 	return {
 		scope: {
-			idLesson: '='
 		},
 		templateUrl: 'views/directives/stars.html',
 		link: link

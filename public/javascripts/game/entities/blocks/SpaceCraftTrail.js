94f91246b2d9baff053fd760c9de966ca29bceaa
vaimer <overespada@gmail.com>
2016-07-18 11:25:56 +0300

    fix

--- public/javascripts/game/entities/blocks/SpaceCraftTrail.js
+++ public/javascripts/game/entities/blocks/SpaceCraftTrail.js
@@ -12,12 +12,12 @@ function SpaceCraftTrail (game, spaceCraft){
 	var sprite = spaceCraft.sprite;
 
 	t.emitter = game.add.emitter(0, 0, 10);
-	t.emitter.makeParticles('fire');
+	t.emitter.makeParticles('trail');
 
 	sprite.addChild(t.emitter);
 
 	t.emitter.y = 0;
-	t.emitter.x = -23;
+	t.emitter.x = - sprite.width / 2;
 	t.emitter.lifespan = 100;
 	t.emitter.maxParticleSpeed = new Phaser.Point(-50,25);
 	t.emitter.minParticleSpeed = new Phaser.Point(-100,-25);

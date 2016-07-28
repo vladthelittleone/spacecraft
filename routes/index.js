90ce0d0d4120eedb773cfe041378148b7e74380e
Skurishin Vladislav <vskurishin@marathonbet.ru>
2016-07-28 15:23:08 +0300

    fix secure issue

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@
  * @since 03.02.16
  * @author Skurishin Vladislav
  */
-const users = require('./users');
 const login = require('./login');
 const user = require('./user');
 const main = require('./main');
@@ -15,7 +14,6 @@ module.exports = function (app)
 {
 	// Мидлвер
 	app.use('/', main);
-	app.use('/users', users);
 	app.use('/login', login);
 	app.use('/user', user);
 	app.use('/reg', reg);

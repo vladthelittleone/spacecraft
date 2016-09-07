e125b2024a2e15c02825a19391797754187595ef
Skurishin Vladislav <vskurishin@marathonbet.ru>
2016-09-07 19:22:17 +0300

    issue#345 add new dir

--- public/javascripts/directives/diagram.directive.js
+++ public/javascripts/directives/diagram.directive.js
@@ -15,40 +15,50 @@ module.exports = Diagram;
 function Diagram() {
 
 	var directive = {
-		templateUrl: 'views/directives/diagram.html',
-		link:        link,
-		restrict:    'EA'
+		link: link,
+		restrict: 'E',
+		scope: {
+			height: '=',
+			width: '=',
+			gridSize: '=',
+			graph: '='
+		}
 	};
 
 	return directive;
 
-	function link() {
+	function link(scope, element) {
 
-		var graph = new joint.dia.Graph;
+		var diagram = newDiagram(scope.height, scope.width, scope.gridSize, scope.graph, element[0]);
 
-		var paper = new joint.dia.Paper({
-			el: $('#diagram'),
-			width: 600,
-			height: 200,
-			model: graph,
-			gridSize: 1
+		//add event handlers to interact with the diagram
+		diagram.on('cell:pointerclick', function (cellView, evt, x, y) {
+
+			//your logic here e.g. select the element
+
 		});
 
-		var rect = new joint.shapes.basic.Rect({
-			position: { x: 100, y: 30 },
-			size: { width: 100, height: 30 },
-			attrs: { rect: { fill: 'blue' }, text: { text: 'my box', fill: 'white' } }
+		diagram.on('blank:pointerclick', function (evt, x, y) {
+
+			// your logic here e.g. unselect the element by clicking on a blank part of the diagram
 		});
 
-		var rect2 = rect.clone();
-		rect2.translate(300);
+		diagram.on('link:options', function (evt, cellView, x, y) {
 
-		var link = new joint.dia.Link({
-			source: { id: rect.id },
-			target: { id: rect2.id }
+			// your logic here: e.g. select a link by its options tool
 		});
 
-		graph.addCells([rect, rect2, link]);
+	}
+
+	function newDiagram(height, width, gridSize, graph, targetElement) {
+
+		return new joint.dia.Paper({
+			el:       targetElement,
+			width:    width,
+			height:   height,
+			gridSize: gridSize,
+			model:    graph
+		});
 
 	}
 

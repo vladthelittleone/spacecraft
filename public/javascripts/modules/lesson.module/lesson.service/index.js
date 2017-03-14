aba7669daff13d776b0a04d86398c20357951479
vaimer <overespada@gmail.com>
2017-03-14 22:47:26 +0300

    resolve problem

--- public/javascripts/modules/lesson.module/lesson.service/index.js
+++ public/javascripts/modules/lesson.module/lesson.service/index.js
@@ -53,6 +53,9 @@ function LessonService(connection,
 	// Текущий - это именно тот, который в данный момент проходит пользователь.
 	var currentLessonStatistics = null;
 
+	// Флаг показывающий проходился ли уже ранее данный урок.
+	var isCurrentLessonCompleted = false;
+
 	that.setEditorSession = setEditorSession;
 	that.setMarkerId = setMarkerId;
 
@@ -354,7 +357,7 @@ function LessonService(connection,
 		++scope.subIndex;
 
 		// Сохраняем статистику текущего положения по уроку.
-		saveStatisticsWrapper(scope.subIndex, false);
+		saveStatisticsWrapper(scope.subIndex, isCurrentLessonCompleted);
 
 		CodeLauncher.stop();
 
@@ -415,6 +418,7 @@ function LessonService(connection,
 
 		// Выводим доску оценки подурока
 		scope.lessonIsCompleted = true;
+		isCurrentLessonCompleted = true;
 
 		// Вызываем коллбэки, которые подписались на скрытие вкладки,
 		// так как на данном этапе урок закончен, и можно считать,
@@ -488,6 +492,12 @@ function LessonService(connection,
 			// Запоминаем ссылку на данные по урокам, которые выгрузили с сервера.
 			lessons = data.lessons || [];
 			totalFinalScore = data.totalFinalScore || 0;
+
+			if(data.completed) {
+
+				isCurrentLessonCompleted =  true;
+
+			}
 
 			prepareLesson(getCurrentLesson());
 

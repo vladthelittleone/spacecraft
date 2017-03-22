4b91c62509d6e78de237cfa6fdc43a26db4073e9
iretd <zanzarah92@gmail.com>
2017-03-22 15:15:39 +0300

    Removed field 'userPosition' from leaderBoard response. Intoduced field 'isItMe' only for current user

--- utils/help/statistics/leaderBoard.js
+++ utils/help/statistics/leaderBoard.js
@@ -11,38 +11,48 @@ module.exports = LeaderBoard();
 
 function LeaderBoard() {
 
-	var t = {};
+	let t = {};
 
-	t.findUserPosition = findUserPosition;
+	t.tryToMarkUser = tryToMarkUser;
 	t.prepareForResponse = prepareForResponse;
 
 	return t;
 
 	/**
-	 * Реализация поиска пользователя в таблице leaderBoard'a.
+	 * Реализация пометки пользователя в таблице leaderBoard'a.
+	 * Процесс пометки представляет из себя определение свойства isItMe со значением true
+	 * для искомого юзера.
 	 * Ожидается, что таблица была получена из mongodb -> поле idUser в leaderBoard имеет
 	 * тип ObjectID.
 	 * @param args.idUser - 24 byte hex string idUser'a, которая кастуется к ObjectId.
 	 * @param args.leaderBoard - таблица лидеров выдернутая из mongodb.
 	 * @returns {number}
 	 */
-	function findUserPosition(args) {
+	function tryToMarkUser(args) {
 
 		// Осуществляем поиск позиции пользователя в leaderBoard'e по полю idUser
-		return lodash.findIndex(args.leaderBoard, {idUser: new ObjectID(args.idUser)});
+
+		let user = lodash.find(args.leaderBoard, {idUser: new ObjectID(args.idUser)});
+
+		if (user) {
+
+			user.isItMe = true;
+
+		}
 
 	}
 
 	/**
 	 * Подготовка leaderBoard'a к выдаче пользователю в качестве ответа на запрос.
-	 * Подготовка включает в себя оставление ТОЛЬКО полей 'totalFinalScore' и 'name'.
+	 * Подготовка включает в себя оставление ТОЛЬКО полей 'totalFinalScore',  'name' и
+	 * 'isItMe' (будет определено для текущего пользователя в таблице).
 	 */
 	function prepareForResponse(leaderBoard) {
 
 		// Избавляемся от поля idUser в элементах leaderBoard перед выдачей.
 		lodash.forEach(leaderBoard, function(record, index) {
 
-			leaderBoard[index] = lodash.pick(record, ['totalFinalScore', 'name']);
+			leaderBoard[index] = lodash.pick(record, ['totalFinalScore', 'name', 'isItMe']);
 
 		});
 

886fa381b6bc5c924e5b534521aacf2238d316ae
iretd <zanzarah92@gmail.com>
2017-04-28 22:38:30 +0300

    issue#401 - 'yandex metrika' added (rate lesson)

---
+++ public/javascripts/utils/analytics.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/**
+ * Модуль аналитики.
+ * Инкапсулирует в себя вопросы реализации вопросов аналитики
+ * действий пользователя на сайте.
+ *
+ * @since 28.04.17
+ * @author iretd
+ */
+
+module.exports = Analytics();
+
+function Analytics() {
+
+	var that = {};
+
+	that.rateLesson = rateLesson;
+
+	return that;
+
+	/**
+	 * Метод аналитики выставления оценки урока.
+     */
+	function rateLesson(lessonId, rating) {
+
+		var lessonRating = {};
+		lessonRating['Урок ' + lessonId] = rating;
+
+		yaCounter43763714.params({'Оценки уроков' : lessonRating});
+
+	}
+
+}

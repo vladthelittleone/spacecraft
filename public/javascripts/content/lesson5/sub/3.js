07c69a8ed2d87af971faf17e64854c3ebd4c7619
Skurishin Vladislav <vladthelittleone@gmail.com>
2017-05-08 18:05:00 +0300

    issue#436 fix text of 3 sublesson

--- public/javascripts/content/lesson5/sub/3.js
+++ public/javascripts/content/lesson5/sub/3.js
@@ -7,80 +7,57 @@ module.exports = OperatorAssignment();
 /**
  * Урок - 'Догнать за 64 секунды';
  */
-function OperatorAssignment () {
+function OperatorAssignment() {
 
 	return {
-		isRestartDisabled: true,
-		title: 'В присваивании нет ничего плохого',
-		content: content,
+		isRestartDisabled:  true,
+		title:              'В присваивании нет ничего плохого',
+		content:            content,
 		interpreterHandler: interpreterHandler,
-		instructions: '<ul>' +
-		'<li>Для запуска кода нажмите, в правом верхнем углу, на зеленую кнопку ' +
-		'<i class="glyphicon glyphicon-play green"></i>.</li>' +
-		'<li>Присвой себе больше знаний: ' +
-		'<a target="_blank" href="https://msdn.microsoft.com/ru-ru/library/1w2h1k9x(v=vs.94).aspx">клац</a>.</li>' +
-		'</ul>',
-		character: [{
-			audio: 'audio/lesson2/3-2',
-			css: 'astromen-img',
+		instructions:       '<ul>' +
+							'<li>Для запуска кода нажмите, в правом верхнем углу, на зеленую кнопку ' +
+							'<i class="glyphicon glyphicon-play green"></i>.</li>' +
+							'<li>Присвой себе больше знаний: ' +
+							'<a target="_blank" href="https://msdn.microsoft.com/ru-ru/library/1w2h1k9x(v=vs.94).aspx">клац</a>.</li>' +
+							'</ul>',
+		character:          [{
+			audio:  'audio/lesson2/3-2',
+			css:    'astromen-img',
 			marker: {
 				x1: 3,
-				y2: Infinity
-			}
-		}, {
-			audio: 'audio/lesson2/3-2',
-			css: 'astromen-img',
-			marker: {
-				x1: 7,
-				y2: Infinity
-			}
-		}, {
-			audio: 'audio/lesson2/3-2',
-			css: 'astromen-img',
-			marker: {
-				x1: 11,
-				y2: Infinity
-			}
-		}, {
-			audio: 'audio/lesson2/3-2',
-			css: 'astromen-img',
-			marker: {
-				x1: 14,
 				y2: Infinity
 			}
 		}]
 	};
 
-	function content () {
+	function content() {
 
 		return '<p>Рассмотрим оператор присваивания более подробно.' +
-			'<p>Напомню, что с  его помощью переменной задают значение. Например, вы можете создать переменную, ' +
-			'содержащую ответ на «Главный вопрос жизни, вселенной и всего такого».</p>' +
-			'<pre><strong>var</strong> ultimateQuestionAnswer = 42;</pre> ' +
-			'Переменной будет присвоено значение <class="under-label">42</span>. ' +
-			'Любой переменной можно присвоить результат выполнения выражения. Например:' +
-			'<pre><strong>var</strong> ultimateSoloAnswer = ultimateQuestionAnswer * 322 / 21;</pre>' +
-			'В результате переменной <span class="under-label">ultimateSoloAnswer</span> будет присвоено значение <strong>644</strong>.' +
-			'Однако, в случае с оператором <span class="under-label">=</span>, сперва будет вычислено выражение справа от оператора ' +
+			'<p>Напомним, что с  его помощью переменной задают значение. Например, вы можете создать переменную, ' +
+			'содержащую ответ на «Главный вопрос жизни, вселенной и всего такого»:</p>' +
+			'<pre><strong>var</strong> ultimateAnswer = 42;</pre>' +
+			'<p>Любой переменной можно присвоить результат выполнения выражения. Например:</p>' +
+			'<pre><strong>var</strong> solo = 301 + ultimateAnswer / 2;</pre>' +
+			'<p>В результате переменной <span class="under-label">solo</span> будет присвоено значение <strong>322</strong>. ' +
+			'Заметим, что сперва будет выполнено деление, затем сложение ' +
 			'и только после этого результат вычисления будет присвоен переменной.</p>' +
-			'<p>Вы знаете почему так происходит?</p>';
+			'<p>Догадываетесь почему так происходит?</p>';
 
 	}
 
-	function interpreterHandler (value) {
+	function interpreterHandler(value) {
 
 		var correctText;
 
 		if (value) {
 
-			correctText = '<p>Присваивание это немного скучно, пока присваивание не начинает выполнять BBot.</p>' +
-				'<p>Транслирую:</p>';
+			correctText = '';
 
 			value.forEach(function (v) {
 
 				if (v) {
 
-					correctText += '<p class="bbot-output">' + v + '</p>';
+					correctText += v + '<br>';
 
 				}
 
@@ -89,9 +66,14 @@ function OperatorAssignment () {
 		}
 
 		var lessonResults = LessonResults({
-											  correct: correctText,
-											  unknownError: '<p>Тададам! И вновь 0шибка!</p>'
-										  });
+
+			correct: '<p>Solo Yolo:</p>' +
+					 '<p class="bbot-output">' + correctText + '</p>',
+
+			unknownError: '<p>И это была твоя последняя ошибка!</p>' +
+						  '<p>Ладно, предпоследняя!</p>'
+
+		});
 
 		if (correctText) {
 

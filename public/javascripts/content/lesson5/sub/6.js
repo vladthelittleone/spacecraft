7c8a6ce32ad8957b354edb47be8c72f714366294
Skurishin Vladislav <vladthelittleone@gmail.com>
2017-05-16 12:00:27 +0300

    issue#446 fix some issues

--- public/javascripts/content/lesson5/sub/6.js
+++ public/javascripts/content/lesson5/sub/6.js
@@ -18,7 +18,7 @@ function OperatorIncrement() {
 							'<li>Для запуска кода нажмите, в правом верхнем углу, на зеленую кнопку ' +
 							'<i class="glyphicon glyphicon-play green"></i>.</li>' +
 							'<li>Инкрементируй свои знания: ' +
-							'<a target="_blank" href="https://msdn.microsoft.com/ru-ru/library/26k41698(v=vs.94).aspx">клац</a>.</li>' +
+							'<a target="_blank" href="https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators#Инкремент_()">пау</a>.</li>' +
 							'</ul>',
 		character:          [{
 			audio:  'audio/lesson2/3-2',
@@ -41,16 +41,19 @@ function OperatorIncrement() {
 
 		return '<p>Часто пилоты-инженеры используют операцию изменения значения числовой переменной на ' +
 			'<strong>1</strong>. В связи с этим, в языке программирования <strong>JavaScript</strong> существуют ' +
-			'специальные операторы: <p> 1. Инкремент <strong class="under-label">++</strong> - увеличивает значение на ' +
-			'<strong>1</strong>.<p> 2. Декремент <strong class="under-label">--</strong> - уменьшает значение на ' +
-			'<strong>1</strong>. <p>Существуют две формы записи данных операторов: до переменной («префиксная» форма) ' +
+			'специальные операторы:</p>' +
+			'<ul>' +
+			'<li>Инкремент <strong class="under-label">++</strong> - увеличивает значение на <strong>1</strong>;</li>' +
+			'<li>Декремент <strong class="under-label">--</strong> - уменьшает значение на <strong>1</strong>.</li>' +
+			'</ul>' +
+			'<p>Существуют две формы записи данных операторов: до переменной («префиксная» форма) ' +
 			'и после переменной («постфиксная» форма). Разница между ними видна только в том случае, когда мы хотим ' +
-			'использовать результат операции. Рассмотрим пример в редакторе кода.</p> Если оператор ' +
-			'<strong class="under-label">++</strong> или <strong class="under-label">--</strong> стоит перед ' +
+			'использовать результат операции. Рассмотрим пример в редакторе кода.</p>' +
+			'<p>Если оператор <strong class="under-label">++</strong> или <strong class="under-label">--</strong> стоит перед ' +
 			'переменной, то сперва произойдет изменение переменной <strong>speed</strong>, а уже затем получившееся ' +
 			'значение будет присвоено переменной <strong>result</strong>. Но, если оператор стоит после переменной ' +
 			'<strong>speed</strong>, то сначала ее значение будет присвоено переменной <strong>result</strong>, ' +
-			'а уже потом изменено на <strong>1</strong>.';
+			'а уже потом изменено на <strong>1</strong>.</p>';
 
 	}
 
